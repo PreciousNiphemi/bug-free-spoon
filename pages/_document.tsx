17ba01dd4be93351e2b6200c1ee762feb16940bd
PreciousNiphemi <preciousoluwanifemi1999@gmail.com>
2022-03-13 13:10:55 +0100

    🐛 fix(fonts):  fix kenoky font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,11 +5,11 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link href="http://fonts.cdnfonts.com/css/kenoky" rel="stylesheet" />
-          <link
+          <link rel="stylesheet" href="http://fonts.cdnfonts.com/css/kenoky" />
+          {/* <link
             href="https://fonts.googleapis.com/css2?family=Montserrat&display=optional"
             rel="stylesheet"
-          />
+          /> */}
         </Head>
         <body>
           <Main />
